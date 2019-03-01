8f48bcd663f17889c16881a30ce7bae2eae98bc2
Matteo Muscas <thatman.j@gmail.com>
2019-03-01 10:10:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,8 +9,8 @@ class CreateCostumeStore < ActiveRecord::Migration
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
-      opening_time
-      closing_time
+      t.string :opening_time
+      t.string :closing_time
       t.timestamp
     end
   end
