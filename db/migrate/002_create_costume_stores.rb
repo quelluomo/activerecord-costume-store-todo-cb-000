55e20122dc386cecce8614aa7da563a170d27373
Matteo Muscas <thatman.j@gmail.com>
2019-03-01 10:06:35 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStore < ActiveRecord::Migration
+
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.string :image_url
+      t.timestamp
+    end
+  end
+
+end
