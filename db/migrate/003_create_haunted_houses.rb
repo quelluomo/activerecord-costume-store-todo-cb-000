d2fad44e4616cf7a01f9c4cdc1c325df158d7615
Matteo Muscas <thatman.j@gmail.com>
2019-03-01 10:19:50 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ class CreateHauntedHouses < ActiveRecord::Migration
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.integer :costume_inventory
+      t.string :theme
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.datetime :opening_time
